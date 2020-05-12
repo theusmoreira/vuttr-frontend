3f280a2da312240c63346fec864d0c7acc0994f6
Matheus Santos Moreira <matheussm301@gmail.com>
2020-05-12 19:53:10 -0300

    Arquivo de rotas

---
+++ src/Routes.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Session from './pages/Session';
+
+import { isAuthenticated } from "./services/auth";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: '/signin', state: { from: props.location } }} />
+      )
+    }
+  />
+);
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/signin' component={SignIn} />
+        <Route path='/signup' component={SignUp} />
+        <PrivateRoute path='/tools' component={Session} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
