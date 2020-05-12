facb16a52bded87d3947b9d2a304f69cdce8a7b9
Matheus Santos Moreira <matheussm301@gmail.com>
2020-05-12 19:54:03 -0300

    Criação de pagina de sessão

---
+++ src/pages/Session/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import 'styles.css';
+
+class Session extends Component {
+  render() {
+    return (
+      <h1>OLa mundo</h1>
+    );
+  }
+}
+
+export default Session;
