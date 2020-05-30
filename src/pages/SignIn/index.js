5dea91696b2c3484d915ed821ba48765c05713e8
Matheus Santos Moreira <matheussm301@gmail.com>
2020-05-30 09:19:59 -0300

    Transfromando em function component

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { FaChevronRight } from 'react-icons/fa'
 import { Link, withRouter } from 'react-router-dom';
 
@@ -7,60 +7,54 @@ import { login } from '../../services/auth';
 
 import './styles.css';
 
-class SignIn extends Component {
-  state = {
-    name: '',
-    password: '',
-    error: ''
-  };
+function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-  handleSignIn = async e => {
+  const handleSignIn = async e => {
     e.preventDefault();
 
-    const { email, password } = this.state;
     if (!email || !password) {
-      this.setState({ error: 'Preencha e-mail e senha para continuar!' });
+      setError('Preencha e-mail e senha para continuar!');
     } else {
       try {
         const response = await api.post('/login', { email, password });
         login(response.data.token);
         this.props.history.push('/tools');
       } catch (error) {
-        this.setState({
-          error: 'Houve um problema com o login, verifique suas credenciais.'
-        });
+        setError('Houve um problema com o login, verifique suas credenciais.'
+        );
       }
     }
   };
 
-  render() {
-    return (
-      <div className="container-signin">
-        <div className="content-signin">
-          <section className='form'>
-            <form onSubmit={this.handleSignIn}>
-              <h1>Sign In</h1>
-              {this.state.error && <p>{this.state.error}</p>}
-              <input
-                type='email'
-                placeholder='Seu Email'
-                onChange={e => this.setState({ email: e.target.value })}
-              />
-              <input
-                type='password'
-                placeholder='Senha'
-                onChange={e => this.setState({ password: e.target.value })}
-              />
-              <button className='button' type='submit'>Entrar</button>
-              <Link className="back-link" to="/signup">
-                <FaChevronRight size={16} color='#170C3A' />
-                <span>Não tenho cadastro</span>
-              </Link>
-            </form>
-          </section>
-        </div>
+  return (
+    <div className="container-signin">
+      <div className="content-signin">
+        <section className='form'>
+          <form onSubmit={handleSignIn}>
+            <h1>Sign In</h1>
+            {error && <p>{error}</p>}
+            <input
+              type='email'
+              placeholder='Seu Email'
+              onChange={e => setEmail({ email: e.target.value })}
+            />
+            <input
+              type='password'
+              placeholder='Senha'
+              onChange={e => setPassword({ password: e.target.value })}
+            />
+            <button className='button' type='submit'>Entrar</button>
+            <Link className="back-link" to="/signup">
+              <FaChevronRight size={16} color='#170C3A' />
+              <span>Não tenho cadastro</span>
+            </Link>
+          </form>
+        </section>
       </div>
-    );
-  }
+    </div>
+  );
 }
 export default withRouter(SignIn);
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { FaChevronLeft } from 'react-icons/fa'
 
@@ -7,65 +7,63 @@ import { login } from '../../services/auth';
 
 import './styles.css'
 
-class SignUp extends Component {
-  state = {
-    username: "",
-    email: "",
-    password: "",
-    error: ""
-  }
+function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-  handleRegister = async e => {
+  const handleRegister = async e => {
     e.preventDefault();
-    const { name, email, password } = this.state;
+
     if (!name || !email || !password) {
-      this.setState({ error: 'Preencha todos os dados para se cadastrar' });
+      setError('Preencha todos os dados para se cadastrar');
     } else {
       try {
-        const response = await api.post('users', {name, email, password});
+        const response = await api.post('users', { name, email, password });
         login(response.data.token);
+        console.log(response.data.token)
         this.props.history.push('/tools');
       } catch (error) {
         console.log(error);
-        this.setState({ error: "Ocorreu um erro ao registrar sua conta. T.T" });
+        setError('Ocorreu um erro ao registrar sua conta. :-/');
       }
     }
   }
 
-  render() {
-    return (
-      <div className="container-signup">
-        <div className="content-signup">
-          <section>
-            <h1>Cadastro</h1>
-            <p>Faça seu cadastro e sempre se lembre da suas ferramentas mais usadas</p>
-            <Link className='back-link' to='/'>
-              <FaChevronLeft size={25} color='#170C3A' />
-              <span>Fazer login</span>
-            </Link>
 
-            <form onSubmit={this.handleRegister}>
-            {this.state.error && <p>{this.state.error}</p>}
-              <input
-                placeholder='Seu nome'
-                onChange={e => this.setState({ name: e.target.value })}
-              />
-              <input
-                placeholder='Email'
-                onChange={e => this.setState({ email: e.target.value })}
-              />
-              <input
-                placeholder='Senha'
-                type='password'
-                onChange={e => this.setState({ password: e.target.value })}
-              />
-              <button className='button' type='submit'>Cadastrar</button>
-            </form>
-          </section>
-        </div>
+  return (
+    <div className="container-signup">
+      <div className="content-signup">
+        <section>
+          <h1>Cadastro</h1>
+          <p>Faça seu cadastro e sempre se lembre da suas ferramentas mais usadas</p>
+          <Link className='back-link' to='/'>
+            <FaChevronLeft size={25} color='#170C3A' />
+            <span>Fazer login</span>
+          </Link>
+
+          <form onSubmit={handleRegister}>
+            {error && <p>{error}</p>}
+            <input
+              placeholder='Seu nome'
+              onChange={e => setName({ name: e.target.value })}
+            />
+            <input
+              placeholder='Email'
+              onChange={e => setEmail({ email: e.target.value })}
+            />
+            <input
+              placeholder='Senha'
+              type='password'
+              onChange={e => setPassword({ password: e.target.value })}
+            />
+            <button className='button' type='submit'>Cadastrar</button>
+          </form>
+        </section>
       </div>
-    )
-  }
-}
+    </div>
+  )
+};
 
 export default withRouter(SignUp);
