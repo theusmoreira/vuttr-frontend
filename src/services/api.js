5bb5fde19f9d02264056052f558ed128e3e4e270
Matheus Santos Moreira <matheussm301@gmail.com>
2020-05-31 16:13:21 -0300

    Usando api em desenvolvimento

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: 'https://vuttr-node.herokuapp.com'
+  baseURL: 'http://localhost:3333',
 });
 
 api.interceptors.request.use(async config => {
